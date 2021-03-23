da01d79f1423c869d51737df24851c39de36b8da
shubhvash <shubhvash87@gmail.com>
2021-03-23 07:50:01 +0530

    signout fix

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
       email: data.email,
       entries: data.entries,
       joined: data.createdAt
-    }})
+    }});
   }
 
   calculateFaceLocation = (data) => {
@@ -81,15 +81,6 @@ class App extends Component {
     this.setState({imageUrl: this.state.input});
     app.models
       .predict(
-        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-        // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
-        // so you would change from:
-        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        // to:
-        // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
       .then(response => {
@@ -143,7 +134,7 @@ class App extends Component {
               <FaceRecognition box={box} imageUrl={imageUrl} />
             </div>
           : (
-             route === 'signin'
+             route === 'signin' || route === 'signout'
              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
             )
